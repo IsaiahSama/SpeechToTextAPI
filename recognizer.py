96875210e002d5ffc7042334e28ffe49cedf0efd
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-05 17:41:19 -0400

    Added recognizer.py and test audio

---
+++ recognizer.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+
+from os import system
+
+def transcribe(audio_file):
+    AUDIO_FILE = f"./audios/{audio_file}"
+
+    system(fr"ffmpeg -i {AUDIO_FILE} ./audios/output.wav")
+
+    r = sr.Recognizer()
+    with sr.AudioFile("./audios/output.wav") as source:
+        audio = r.record(source)
+
+    try:
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+if __name__ == "__main__":
+    transcribe("test.mp3")
