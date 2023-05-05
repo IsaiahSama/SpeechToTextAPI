92b7c66db475ac1b1118bebaed5af66915edb519
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-05 17:14:55 -0400

    Added core files

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/transcribe", methods=["POST"])
+def transcribe():
+    print("TRANSCRIBE ROUTE HIT!")
